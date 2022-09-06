ea44fa88167e62a4a9bfcc4f179f7e0bab2e2dad
Gabriel Santana <112592419+GabrielTHMx@users.noreply.github.com>
2022-09-06 16:44:20 -0500

    Add files via upload
    Este proyecto es una ejecución simple de la concatenación de cadenas de caracteres

---
+++ Primer_proyecto_Concatenacion_strings.py
@@ -0,0 +1,8 @@
+print("Hello world I say 'Hello'")
+print("I love to study Python")
+print("Studying with 'Python Total' is very funny")
+print("\tHello " + 'Gabriel\n\tYou\'re welcome!')
+print("Your name is: " + input("Enter name: ") + " " + input("Enter last name: "))
+
+print("Your beer name is:\n\" " + input("Enter your favorite color: ") + " " + input("Enter your favorite age: ")
+      + " \"")
